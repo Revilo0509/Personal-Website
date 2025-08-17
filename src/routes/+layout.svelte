93b5d8bf45241d7125d6c4757f50afa2f147ae57
Revilo0509 <olivers_gamecenter@outlook.com>
2025-08-17 21:45:14 +0200

    inital commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,11 @@
 <script>
-    import "$lib/global.css";
-    import "$lib/components/MyBtn.svelte";
-    import MyBtn from "$lib/components/MyBtn.svelte";
-    import Background from "$lib/components/Background.svelte";
+	import favicon from '$lib/assets/favicon.svg';
 
-    let { children } = $props()
+	let { children } = $props();
 </script>
 
-<Background />
-<header>
-    <div class="headerLinksContainer">
-        <ul class="headerLinks">
-            <MyBtn href="/">Home</MyBtn>
-            <MyBtn href="/about">About</MyBtn>
-            <MyBtn href="/socials">Socials</MyBtn>
-            <MyBtn href="/projects">Projects</MyBtn>
-        </ul>
-    </div>
-</header>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
 
-<main>
-    {@render children()}
-</main>
+{@render children?.()}
