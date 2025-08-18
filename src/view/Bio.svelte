ffb03e726f293269fad491b82268b3bf83effb7b
Revilo0509 <olivers_gamecenter@outlook.com>
2025-08-18 14:46:31 +0200

    wut

--- src/view/Bio.svelte
+++ src/view/Bio.svelte
@@ -1,12 +1,14 @@
 <section id="bio">
-  <div class="Box">Hello</div>
+  <div class="Box">Hi, I'm Revilo!
+
+I code for fun and know multiple languages. I also love playing games. The languages I know are Python, C++, LUA, HTML, CSS and a bit of Javascript. But I also know English, Swedish, Polish and I'm learning Spanish. I mostly play modded minecraft with friends but I also play other games.</div>
 </section>
 
 <style>
   section {
     display: flex;
   }
-  
+
   .Box {
     margin: 6rem;
     margin-left: auto;
